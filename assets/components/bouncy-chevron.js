243d09b50e5c3ab9b04c2014f2cd8a0336dc3eb0
Jan Syring-Lingenfelder <github@jan-sl.de>
2020-11-28 20:28:36 +0100

    refactor: chevron is independent of the anchor

--- assets/components/bouncy-chevron.js
+++ assets/components/bouncy-chevron.js
@@ -1,8 +1,5 @@
-document.querySelectorAll('.icon.is-clickable').forEach(el => {
-    el.addEventListener('click', function(e) { // no arrow function here because of the binding context of 'this'!
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+document.querySelector('.icon.is-clickable').addEventListener('click', () => {
+    document.querySelector('main').scrollIntoView({
+        behavior: 'smooth'
     });
 });
