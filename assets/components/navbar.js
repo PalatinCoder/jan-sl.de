aa143cbcf39d18f535136e23343464be9c14da7e
Jan Syring-Lingenfelder <github@jan-sl.de>
2020-12-19 21:57:54 +0100

    feat: single post template

---
+++ assets/components/navbar.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.navbar-burger').forEach(burger => {
+    burger.addEventListener('click', () => {
+        const $target = document.getElementById(burger.dataset.target);
+        burger.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+    });
+});
